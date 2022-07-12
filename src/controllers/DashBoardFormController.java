4b959a372115457619d2a3c6620d583a5ad667f2
sajithmadusamkha <sajithmadusankha02@gmail.com>
2022-07-12 10:55:04 +0530

    student save controller added

---
+++ src/controllers/DashBoardFormController.java
@@ -0,0 +1,20 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+
+public class DashBoardFormController {
+    public void btnSaveOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnUpdateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDeleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnLoadAllStudentOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSeachStudentOnAction(ActionEvent actionEvent) {
+    }
+}
---
+++ src/controllers/SaveStudentFormController.java
@@ -0,0 +1,17 @@
+package controllers;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+
+public class SaveStudentFormController {
+    public JFXTextField txtId;
+    public JFXTextField txtName;
+    public JFXTextField txtEmail;
+    public JFXTextField txtContactNo;
+    public JFXTextField txtAddress;
+    public JFXTextField txtNic;
+
+    public void btnSaveOnAction(ActionEvent actionEvent) {
+
+    }
+}
